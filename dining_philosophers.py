bc66f8ed2115e7dfff3b0e4e937568de0ba7b953
Ferhat Çelik <ferhatsteel34@gmail.com>
2022-12-25 17:31:15 +0300

    dining_philosophers.py ın son hali şuanlık

--- dining_philosophers.py
+++ dining_philosophers.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import time
+from random import random
+from threading import Semaphore, Thread
 
 import pygame
 import asyncio
@@ -110,7 +112,6 @@ meal_3 = Meal((WIDTH // 2 + 0, HEIGHT // 2 - 10))
 meal_4 = Meal((WIDTH // 2 - 60, HEIGHT // 2 - 15))
 meal_group = pygame.sprite.Group()
 meal_group.add([meal_0, meal_1, meal_2, meal_3, meal_4])
-# DECLEARE MEAL SİZE TEXT GROUP
 
 meal0_size = Text("TEXT", (WIDTH // 2 - 40, HEIGHT // 2 - 50), 10, (0, 0, 0))
 meal1_size = Text("TEXT", (WIDTH // 2 + 40, HEIGHT // 2 - 50), 10, (0, 0, 0))
@@ -131,76 +132,39 @@ philosopher0States = [Character(6, 0, (WIDTH // 2 + 10, HEIGHT // 2 + 30)),
                       Character(6, 5, (WIDTH // 2 + 10, HEIGHT // 2 + 30))]
 
 philosopher_1 = Character(0, 0, (WIDTH // 2 + 90, HEIGHT // 2 + 30))  # mavi bere
-philosopher1States = [Character(0, 0, (WIDTH // 2 + 90, HEIGHT // 2 + 30)),
-                      Character(0, 4, (WIDTH // 2 + 90, HEIGHT // 2 + 30)),
-                      Character(0, 5, (WIDTH // 2 + 90, HEIGHT // 2 + 30))]
-
 philosopher_2 = Character(4, -2, (WIDTH // 2 + 160, HEIGHT // 2 + 100))  # Turuncu saçlı
-philosopher2States = [Character(4, -2, (WIDTH // 2 + 160, HEIGHT // 2 + 100))]
-
 philosopher_3 = Character(10, 1, (WIDTH // 2 + 45, HEIGHT // 2 + 180))  # yeşil uzaylı
-philosopher3States = [Character(10, 1, (WIDTH // 2 + 45, HEIGHT // 2 + 180)),
-                      Character(10, 5, (WIDTH // 2 + 45, HEIGHT // 2 + 180)),
-                      Character(10, 6, (WIDTH // 2 + 45, HEIGHT // 2 + 180))]
-
 philosopher_4 = Character(2, 2, (WIDTH // 2 - 65, HEIGHT // 2 + 100))  # kahverengi saçlı
-philosopher4States = [Character(2, 2, (WIDTH // 2 - 65, HEIGHT // 2 + 100)),
-                      Character(2, 8, (WIDTH // 2 - 65, HEIGHT // 2 + 100)),
-                      Character(2, 7, (WIDTH // 2 - 65, HEIGHT // 2 + 100))]
 
 chopstick_0 = Chopstick(225, (WIDTH // 2 + 0, HEIGHT // 2 - 60))
-chopstick0States = [Chopstick(190, (WIDTH // 2 - 20, HEIGHT // 2 - 60)), # 0ın SOL EL (ihtiyar)
-                    Chopstick(225, (WIDTH // 2 + 0, HEIGHT // 2 - 60)), # 0 ın solundakinin normal konumu
-                    Chopstick(260, (WIDTH // 2 + 20, HEIGHT // 2 - 60))] # mavilinin sağ eli
-
 chopstick_1 = Chopstick(160, (WIDTH // 2 + 55, HEIGHT // 2 - 35))
-chopstick1States = [Chopstick(190, (WIDTH // 2 + 45, HEIGHT // 2 - 60)), # mavilinin sol eli
-                    Chopstick(160, (WIDTH // 2 + 55, HEIGHT // 2 - 35)), # mavilinin solundakinin normali
-                    Chopstick(160, (WIDTH // 2 + 75, HEIGHT // 2 - 35))] # gözlüklünün sağ eli
-
 chopstick_2 = Chopstick(75, (WIDTH // 2 + 40, HEIGHT // 2 + 10))
-chopstick2States = [Chopstick(70, (WIDTH // 2 + 20, HEIGHT // 2 + 10)), # yeşilin sağ eli
-                    Chopstick(75, (WIDTH // 2 + 40, HEIGHT // 2 + 10)), # normali
-                    Chopstick(130, (WIDTH // 2 + 80, HEIGHT // 2 + -5))] # gözlüklünün sol eli
-
 chopstick_3 = Chopstick(15, (WIDTH // 2 - 40, HEIGHT // 2 + 10))
-chopstick3States = [Chopstick(-15, (WIDTH // 2 - 75, HEIGHT // 2 + -5)), # soldaki adamın sağ eli
-                    Chopstick(15, (WIDTH // 2 - 40, HEIGHT // 2 + 10)), # normali
-                    Chopstick(15, (WIDTH // 2 - 20, HEIGHT // 2 + 10))] # yeşilin sol eli
-
 chopstick_4 = Chopstick(290, (WIDTH // 2 - 55, HEIGHT // 2 - 35))
-chopstick4States = [Chopstick(290, (WIDTH // 2 - 75, HEIGHT // 2 - 35)), # soldaki adamın sol eli
-                    Chopstick(290, (WIDTH // 2 - 55, HEIGHT // 2 - 35)), # normali
-                    Chopstick(250, (WIDTH // 2 - 55, HEIGHT // 2 - 65))] # 0 ın sağ eli
+
 
 # TEST ALANI
-chopstick_alpha = Chopstick(45, (WIDTH // 2 - 0, HEIGHT // 2 - 0))
+#chopstick_alpha = Chopstick(45, (WIDTH // 2 - 0, HEIGHT // 2 - 0))
 # Chopsticklerde rotasyona 45 diyince sola 45 derece dönüyor ve tam yukarı bakıyor.
 # 225 ise tam sağa bakıyor.
 # Height2 yi arttırmak aşağıya ve Width/2 yi arttırmak sağa gitmesi demek.
 
 # bu aşağıdaki 2 taneyi kullanmayacağız o7
 # bununla konumları test ettim hangisinin hangisi olduğunu artık biliyoruz.
-chopstick_test = Chopstick(290, (WIDTH // 2 - 75, HEIGHT // 2 - 35))
 
 # ÇALIŞMA ALANI
-#liste de sol1,sağ1,sol2,sağ2 şeklinde ilerlicez.
-chopsticklist = [Chopstick(190, (WIDTH // 2 - 20, HEIGHT // 2 - 60)),  # 0sol
-                 Chopstick(250, (WIDTH // 2 - 55, HEIGHT // 2 - 65)),  # 0sağ
-                 Chopstick(190, (WIDTH // 2 + 45, HEIGHT // 2 - 60)),  # 1sol mavi bere
-                 Chopstick(260, (WIDTH // 2 + 20, HEIGHT // 2 - 60)),  # 1sağ
-                 Chopstick(130, (WIDTH // 2 + 80, HEIGHT // 2 + -5)),  # 2sol turuncu
-                 Chopstick(160, (WIDTH // 2 + 75, HEIGHT // 2 - 35)),  # 2sağ
-                 Chopstick(15, (WIDTH // 2 - 20, HEIGHT // 2 + 10)),   # 3sol yeşil
-                 Chopstick(70, (WIDTH // 2 + 20, HEIGHT // 2 + 10)),   # 3sağ
-                 Chopstick(290, (WIDTH // 2 - 75, HEIGHT // 2 - 35)),  # 4sol kahverengi
-                 Chopstick(-15, (WIDTH // 2 - 75, HEIGHT // 2 + -5))  # 4sağ
-
-                 ]
-
-
-
-
+# liste de sol1,sağ1,sol2,sağ2 şeklinde ilerlicez.
+chopstick_list = [Chopstick(190, (WIDTH // 2 - 20, HEIGHT // 2 - 60)),  # 0sol
+                  Chopstick(250, (WIDTH // 2 - 55, HEIGHT // 2 - 65)),  # 0sağ
+                  Chopstick(190, (WIDTH // 2 + 45, HEIGHT // 2 - 60)),  # 1sol mavi bere
+                  Chopstick(260, (WIDTH // 2 + 20, HEIGHT // 2 - 60)),  # 1sağ
+                  Chopstick(130, (WIDTH // 2 + 80, HEIGHT // 2 + -5)),  # 2sol turuncu
+                  Chopstick(160, (WIDTH // 2 + 75, HEIGHT // 2 - 35)),  # 2sağ
+                  Chopstick(15, (WIDTH // 2 - 20, HEIGHT // 2 + 10)),  # 3sol yeşil
+                  Chopstick(70, (WIDTH // 2 + 20, HEIGHT // 2 + 10)),  # 3sağ
+                  Chopstick(290, (WIDTH // 2 - 75, HEIGHT // 2 - 35)),  # 4sol kahverengi
+                  Chopstick(-15, (WIDTH // 2 - 75, HEIGHT // 2 + -5))  # 4sağ
+                  ]
 
 philosopher_group.add(
     [
@@ -211,7 +175,6 @@ philosopher_group.add(
         philosopher_3,
         philosopher_4,
         chair_3,
-        chopstick_0, chopstick_1, chopstick_2, chopstick_3, chopstick_4, chopstick_test
     ]
 )
 '''
@@ -219,6 +182,7 @@ Yemeklerin altına-üstüne kalan haklar yazmalı.
 chopsticklerin muhtemel konumları kaydedilmeli. if ile kullanım belirlenmeli
 if doğru ise yemekten bir hak eksitilmeli.
 '''
+
 chopstick_group = pygame.sprite.Group()
 chopstick_group.add([chopstick_0, chopstick_1, chopstick_2, chopstick_3, chopstick_4])
 philosophers = [philosopher_0,
@@ -228,7 +192,7 @@ philosophers = [philosopher_0,
                 philosopher_4]
 
 # Bir chopstick in aktif olup olmama durumu listesi. Chopstickler buna göre çizilecek.
-chopstick_activity_list = [0 for _ in range(len(chopsticklist))]
+chopstick_activity_list = [0 for _ in range(len(chopstick_list))]
 
 
 async def visual_main():
@@ -239,19 +203,83 @@ async def visual_main():
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONUP:
                 pass
+
+        # chopstickleri her seferinde kontrol edip ekleyecek kısım
+        active_chopstick_group = pygame.sprite.Group()
+        for i in range(len(chopstick_list)):
+            if chopstick_activity_list[i] == 1:
+                active_chopstick_group.add(chopstick_list[i])
+
         background_group.draw(screen)
         screen.blit(title_text.text_surface, title_text.text_rect)
         meal_group.draw(screen)
         philosopher_group.draw(screen)
-        chopstick_group.draw(screen)
+        # chopstick_group.draw(screen) # chopsticklerin asıl yerini çizer
+        active_chopstick_group.draw(screen)  # chopsticklerin o anki konumlarını çizer.
         pygame.display.update()
         clock.tick(60)
 
-#async def semaphore_main():
+
+class DiningPhilosophers:
+    def __init__(self, number_of_philosophers, meal_size=9):
+        self.meals = [meal_size for _ in range(number_of_philosophers)]  # yemekler ayarlanmış.
+        self.chopsticks = [Semaphore(value=1) for _ in range(number_of_philosophers)]
+        self.status = ['WAITING' for _ in range(number_of_philosophers)]
+        # yeni liste her bir stick için 1 artırıcak. 2 durumu yeme, 1 durumu tek stickle bekleme 0 durumu ise sticksiz.
+        self.chopstick_holders = [0 for _ in range(number_of_philosophers)]
+        self.number_of_philosophers = number_of_philosophers
+
+    def philosopher(self, i):
+        j = (i + 1) % self.number_of_philosophers
+        while self.meals[i] > 0:
+            self.status[i] = '  T  '
+            time.sleep(random.random())
+            self.status[i] = '  _  '
+            # chopstick aldığında sol tarafına alıyor ve tutmaya sol kısımı ekliyor.
+            if self.chopsticks[i].acquire(timeout=1):
+                self.chopstick_holders[i] += 1
+                # SOLUNA CHOPSTICK GELMELİ
+                time.sleep(random.random())
+                # 2. chopstick i alıyor.
+                if self.chopsticks[j].acquire(timeout=1):
+                    self.chopstick_holders[i] += 1
+                    self.status[i] = '  E  '  # yeme durumuna geçiyor.
+                    time.sleep(random.random())
+                    self.meals[i] -= 1  # yemeği azalıyor.
+                    self.chopsticks[j].release()  # chopstick in birini bırakıyor.
+                    self.chopstick_holders[i] -= 1
+                self.chopsticks[i].release()  # diğerini bırakıyor.
+                self.chopstick_holders[i] = 0
+                self.status[i] = '  T  '  # boşta durumuna geçiyor.
 
 
+async def semaphore_main():
+    # n filozof sayısı
+    n = 5
+    # m yemeğin büyüklüğü
+    m = 7
+    dining_philosophers = DiningPhilosophers(n, m)
+    # philosphers listesi içine filozoflar kadar thread oluşturuyor.
+    philosophers = [Thread(target=dining_philosophers.philosopher, args=(i,)) for i in range(n)]
+    # threadleri başlatıyor.
+    for philosopher in philosophers:
+        philosopher.start()
+
+    # burada console da sürekli olarak görsel güncelleme yapılması sağlanmış.
+    # buna cidden ihtiyaç varmı?
+    while sum(dining_philosophers.meals) > 0:
+        print("=" * (n * 5))
+        print("".join(map(str, dining_philosophers.status)), " : ",
+              str(dining_philosophers.status.count('  E  ')))
+        print("".join(map(str, dining_philosophers.chopstick_holders)))
+        print("".join("{:3d}  ".format(m) for m in dining_philosophers.meals), " : ",
+              str(sum(dining_philosophers.meals)))
+        time.sleep(0.1)
+    # threadlerin bitmesi bekleniyor.
+    for philosopher in philosophers:
+        philosopher.join()
 
 
 if __name__ == "__main__":
     asyncio.run(visual_main())
-    #asyncio.run(semaphore_main())
+    asyncio.run(semaphore_main())
