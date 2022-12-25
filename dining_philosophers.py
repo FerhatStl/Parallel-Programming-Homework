185187352819ff9ec7f1eadeabbaf6eee8813afe
Ferhat Çelik <ferhatsteel34@gmail.com>
2022-12-25 04:07:20 +0300

    Çalışıyoz

--- dining_philosophers.py
+++ dining_philosophers.py
@@ -7,8 +7,6 @@ Gördüğüm kadarıyla bu kod sadece görsellerin görünmesi için yapılmış
 lerin konumunu kullanma durumuna göre güncellemesi gerekiyor.
 '''
 
-
-
 # Fixes the File not found error when running from the command line.
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
@@ -32,7 +30,7 @@ class Chair(pygame.sprite.Sprite):
     def __init__(self, image_file, location):
         super().__init__()
         self.image = pygame.image.load(image_file)
-        self.image = pygame.transform.scale(self.image, (self.image.get_width()*4, self.image.get_height()*4))
+        self.image = pygame.transform.scale(self.image, (self.image.get_width() * 4, self.image.get_height() * 4))
         self.rect = self.image.get_rect(center=location)
 
 
@@ -41,8 +39,8 @@ class Character(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.image.load("assets/characters.png")
         self.rect = self.image.get_rect(center=location)
-        self.image = self.image.subsurface(pygame.Rect(abs(state_id)*16, character_id*16, 16, 16))
-        self.image = pygame.transform.scale(self.image, (self.image.get_width()*4, self.image.get_height()*4))
+        self.image = self.image.subsurface(pygame.Rect(abs(state_id) * 16, character_id * 16, 16, 16))
+        self.image = pygame.transform.scale(self.image, (self.image.get_width() * 4, self.image.get_height() * 4))
         if state_id < 0:
             self.image = pygame.transform.flip(self.image, True, False)
         self.direction = "right"
@@ -62,7 +60,7 @@ class Meal(pygame.sprite.Sprite):
     def __init__(self, location):
         super().__init__()
         self.image = pygame.image.load("assets/spaghetti_full.png")
-        self.image = pygame.transform.scale(self.image, (self.image.get_width()*1, self.image.get_height()*1))
+        self.image = pygame.transform.scale(self.image, (self.image.get_width() * 1, self.image.get_height() * 1))
         self.rect = self.image.get_rect(center=location)
 
 
@@ -70,7 +68,7 @@ class Chopstick(pygame.sprite.Sprite):
     def __init__(self, angle, location):
         super().__init__()
         self.image = pygame.image.load("assets/chopstick.png")
-        self.image = pygame.transform.scale(self.image, (self.image.get_width()*0.3, self.image.get_height()*0.3))
+        self.image = pygame.transform.scale(self.image, (self.image.get_width() * 0.3, self.image.get_height() * 0.3))
         self.image = pygame.transform.rotate(self.image, angle)
         self.rect = self.image.get_rect(center=location)
 
@@ -87,66 +85,108 @@ background_group = pygame.sprite.Group()
 background_group.add(
     [
         BackgroundFurniture("assets/floor.png", (x, y))
-        for x in range(0, WIDTH+100, 62) for y in range(0, HEIGHT+100, 46)
+        for x in range(0, WIDTH + 100, 62) for y in range(0, HEIGHT + 100, 46)
     ]
 )
 
-background_group.add(BackgroundFurniture("assets/carpet.png", (WIDTH//2, HEIGHT//2), 12))
-background_group.add(BackgroundFurniture("assets/fireplace.png", (WIDTH//2, 60), 4))
+background_group.add(BackgroundFurniture("assets/carpet.png", (WIDTH // 2, HEIGHT // 2), 12))
+background_group.add(BackgroundFurniture("assets/fireplace.png", (WIDTH // 2, 60), 4))
 background_group.add(BackgroundFurniture("assets/music_player.png", (720, 90), 4))
 background_group.add(BackgroundFurniture("assets/sofa_front.png", (560, 80), 4))
 background_group.add(BackgroundFurniture("assets/sofa_single_right.png", (740, 200), 4))
 background_group.add(BackgroundFurniture("assets/stairs.png", (700, 440), 4, True))
 background_group.add(BackgroundFurniture("assets/desk.png", (170, 120), 3))
-background_group.add(BackgroundFurniture("assets/table_horizontal.png", (WIDTH//2, HEIGHT//2), 4))
+background_group.add(BackgroundFurniture("assets/table_horizontal.png", (WIDTH // 2, HEIGHT // 2), 4))
 
-title_text = Text("Dining Philosophers", (WIDTH//2 - 100, HEIGHT - 50), 24, (200, 255, 200))
+title_text = Text("Dining Philosophers", (WIDTH // 2 - 100, HEIGHT - 50), 24, (200, 255, 200))
 
-meal_0 = Meal((WIDTH//2 - 40, HEIGHT//2 - 50))
-meal_1 = Meal((WIDTH//2 + 40, HEIGHT//2 - 50))
-meal_2 = Meal((WIDTH//2 + 60, HEIGHT//2 - 15))
-meal_3 = Meal((WIDTH//2 + 0, HEIGHT//2 - 10))
-meal_4 = Meal((WIDTH//2 - 60, HEIGHT//2 - 15))
+meal_0 = Meal((WIDTH // 2 - 40, HEIGHT // 2 - 50))
+meal_1 = Meal((WIDTH // 2 + 40, HEIGHT // 2 - 50))
+meal_2 = Meal((WIDTH // 2 + 60, HEIGHT // 2 - 15))
+meal_3 = Meal((WIDTH // 2 + 0, HEIGHT // 2 - 10))
+meal_4 = Meal((WIDTH // 2 - 60, HEIGHT // 2 - 15))
 meal_group = pygame.sprite.Group()
 meal_group.add([meal_0, meal_1, meal_2, meal_3, meal_4])
+# DECLEARE MEAL SİZE TEXT GROUP
 
+meal0_size = Text("TEXT", (WIDTH // 2 - 40, HEIGHT // 2 - 50), 10, (0, 0, 0))
+meal1_size = Text("TEXT", (WIDTH // 2 + 40, HEIGHT // 2 - 50), 10, (0, 0, 0))
+meal2_size = Text("TEXT", (WIDTH // 2 + 60, HEIGHT // 2 - 15), 10, (0, 0, 0))
+meal3_size = Text("TEXT", (WIDTH // 2 + 0, HEIGHT // 2 - 10), 10, (0, 0, 0))
+meal4_size = Text("TEXT", (WIDTH // 2 - 60, HEIGHT // 2 - 15), 10, (0, 0, 0))
 
 philosopher_group = pygame.sprite.Group()
-chair_0 = Chair("assets/chair_front_2.png", (WIDTH//2 - 40, HEIGHT//2 - 110))
-chair_1 = Chair("assets/chair_front_2.png", (WIDTH//2 + 40, HEIGHT//2 - 110))
-chair_2 = Chair("assets/chair_right_2.png", (WIDTH//2 + 130, HEIGHT//2 - 10))
-chair_3 = Chair("assets/chair_back_2.png", (WIDTH//2, HEIGHT//2 + 100))
-chair_4 = Chair("assets/chair_left_2.png", (WIDTH//2 - 130, HEIGHT//2 - 10))
+chair_0 = Chair("assets/chair_front_2.png", (WIDTH // 2 - 40, HEIGHT // 2 - 110))
+chair_1 = Chair("assets/chair_front_2.png", (WIDTH // 2 + 40, HEIGHT // 2 - 110))
+chair_2 = Chair("assets/chair_right_2.png", (WIDTH // 2 + 130, HEIGHT // 2 - 10))
+chair_3 = Chair("assets/chair_back_2.png", (WIDTH // 2, HEIGHT // 2 + 100))
+chair_4 = Chair("assets/chair_left_2.png", (WIDTH // 2 - 130, HEIGHT // 2 - 10))
 
-philosopher_0 = Character(6, 0, (WIDTH//2 + 10, HEIGHT//2 + 30)) # kel
-philosopher0States = [Character(6, 0, (WIDTH//2 + 10, HEIGHT//2 + 30)), Character(6, 4, (WIDTH//2 + 10, HEIGHT//2 + 30)), Character(6, 5, (WIDTH//2 + 10, HEIGHT//2 + 30))]
+philosopher_0 = Character(6, 0, (WIDTH // 2 + 10, HEIGHT // 2 + 30))  # kel
+philosopher0States = [Character(6, 0, (WIDTH // 2 + 10, HEIGHT // 2 + 30)),
+                      Character(6, 4, (WIDTH // 2 + 10, HEIGHT // 2 + 30)),
+                      Character(6, 5, (WIDTH // 2 + 10, HEIGHT // 2 + 30))]
 
-philosopher_1 = Character(0, 0, (WIDTH//2 + 90, HEIGHT//2 + 30)) # mavi bere
-philosopher1States = [Character(0, 0, (WIDTH//2 + 90, HEIGHT//2 + 30)), Character(0, 4, (WIDTH//2 + 90, HEIGHT//2 + 30)), Character(0, 5, (WIDTH//2 + 90, HEIGHT//2 + 30))]
+philosopher_1 = Character(0, 0, (WIDTH // 2 + 90, HEIGHT // 2 + 30))  # mavi bere
+philosopher1States = [Character(0, 0, (WIDTH // 2 + 90, HEIGHT // 2 + 30)),
+                      Character(0, 4, (WIDTH // 2 + 90, HEIGHT // 2 + 30)),
+                      Character(0, 5, (WIDTH // 2 + 90, HEIGHT // 2 + 30))]
 
-philosopher_2 = Character(4, -2, (WIDTH//2 + 160, HEIGHT//2 + 100)) # Turuncu saçlı
-philosopher2States = [Character(4, -2, (WIDTH//2 + 160, HEIGHT//2 + 100))]
+philosopher_2 = Character(4, -2, (WIDTH // 2 + 160, HEIGHT // 2 + 100))  # Turuncu saçlı
+philosopher2States = [Character(4, -2, (WIDTH // 2 + 160, HEIGHT // 2 + 100))]
 
-philosopher_3 = Character(10, 1, (WIDTH//2 + 45, HEIGHT//2 + 180)) # yeşil uzaylı
-philosopher3States = [Character(10, 1, (WIDTH//2 + 45, HEIGHT//2 + 180)), Character(10, 5, (WIDTH//2 + 45, HEIGHT//2 + 180)), Character(10, 6, (WIDTH//2 + 45, HEIGHT//2 + 180))]
+philosopher_3 = Character(10, 1, (WIDTH // 2 + 45, HEIGHT // 2 + 180))  # yeşil uzaylı
+philosopher3States = [Character(10, 1, (WIDTH // 2 + 45, HEIGHT // 2 + 180)),
+                      Character(10, 5, (WIDTH // 2 + 45, HEIGHT // 2 + 180)),
+                      Character(10, 6, (WIDTH // 2 + 45, HEIGHT // 2 + 180))]
 
-philosopher_4 = Character(2, 2, (WIDTH//2 - 65, HEIGHT//2 + 100)) # kahverengi saçlı
-philosopher4States = [Character(2, 2, (WIDTH//2 - 65, HEIGHT//2 + 100)), Character(2, 8, (WIDTH//2 - 65, HEIGHT//2 + 100)), Character(2, 7, (WIDTH//2 - 65, HEIGHT//2 + 100)) ]
+philosopher_4 = Character(2, 2, (WIDTH // 2 - 65, HEIGHT // 2 + 100))  # kahverengi saçlı
+philosopher4States = [Character(2, 2, (WIDTH // 2 - 65, HEIGHT // 2 + 100)),
+                      Character(2, 8, (WIDTH // 2 - 65, HEIGHT // 2 + 100)),
+                      Character(2, 7, (WIDTH // 2 - 65, HEIGHT // 2 + 100))]
 
-chopstick_0 = Chopstick(225, (WIDTH//2 + 0, HEIGHT//2 - 60))
-chopstick0States = [Chopstick(190, (WIDTH//2 - 20, HEIGHT//2 - 60)), Chopstick(225, (WIDTH//2 + 0, HEIGHT//2 - 60)), Chopstick(260, (WIDTH//2 + 20, HEIGHT//2 - 60))]
+chopstick_0 = Chopstick(225, (WIDTH // 2 + 0, HEIGHT // 2 - 60))
+chopstick0States = [Chopstick(190, (WIDTH // 2 - 20, HEIGHT // 2 - 60)), # 0ın SOL EL
+                    Chopstick(225, (WIDTH // 2 + 0, HEIGHT // 2 - 60)), # 0 ın solundakinin normal konumu
+                    Chopstick(260, (WIDTH // 2 + 20, HEIGHT // 2 - 60))] # mavilinin sağ eli
 
-chopstick_1 = Chopstick(160, (WIDTH//2 + 55, HEIGHT//2 - 35))
-chopstick1States = [Chopstick(190, (WIDTH//2 + 45, HEIGHT//2 - 60)), Chopstick(160, (WIDTH//2 + 55, HEIGHT//2 - 35)), Chopstick(160, (WIDTH//2 + 75, HEIGHT//2 - 35))]
+chopstick_1 = Chopstick(160, (WIDTH // 2 + 55, HEIGHT // 2 - 35))
+chopstick1States = [Chopstick(190, (WIDTH // 2 + 45, HEIGHT // 2 - 60)), # mavilinin sol eli
+                    Chopstick(160, (WIDTH // 2 + 55, HEIGHT // 2 - 35)), # mavilinin solundakinin normali
+                    Chopstick(160, (WIDTH // 2 + 75, HEIGHT // 2 - 35))] # gözlüklünün sağ eli
 
-chopstick_2 = Chopstick(75, (WIDTH//2 + 40, HEIGHT//2 + 10))
-chopstick2States = [Chopstick(70, (WIDTH//2 + 20, HEIGHT//2 + 10)), Chopstick(75, (WIDTH//2 + 40, HEIGHT//2 + 10)), Chopstick(130, (WIDTH//2 + 80, HEIGHT//2 + -5))]
+chopstick_2 = Chopstick(75, (WIDTH // 2 + 40, HEIGHT // 2 + 10))
+chopstick2States = [Chopstick(70, (WIDTH // 2 + 20, HEIGHT // 2 + 10)), # yeşilin sağ eli
+                    Chopstick(75, (WIDTH // 2 + 40, HEIGHT // 2 + 10)), # normali
+                    Chopstick(130, (WIDTH // 2 + 80, HEIGHT // 2 + -5))] # gözlüklünün sol eli
 
-chopstick_3 = Chopstick(15, (WIDTH//2 - 40, HEIGHT//2 + 10))
-chopstick3States = [Chopstick(-15, (WIDTH//2 - 75, HEIGHT//2 + -5)), Chopstick(15, (WIDTH//2 - 40, HEIGHT//2 + 10)), Chopstick(15, (WIDTH//2 - 20, HEIGHT//2 + 10))]
+chopstick_3 = Chopstick(15, (WIDTH // 2 - 40, HEIGHT // 2 + 10))
+chopstick3States = [Chopstick(-15, (WIDTH // 2 - 75, HEIGHT // 2 + -5)), # soldaki adamın sağ eli
+                    Chopstick(15, (WIDTH // 2 - 40, HEIGHT // 2 + 10)), # normali
+                    Chopstick(15, (WIDTH // 2 - 20, HEIGHT // 2 + 10))] # yeşilin sol eli
 
-chopstick_4 = Chopstick(290, (WIDTH//2 - 55, HEIGHT//2 - 35))
-chopstick4States = [Chopstick(290, (WIDTH//2 - 75, HEIGHT//2 - 35)), Chopstick(290, (WIDTH//2 - 55, HEIGHT//2 - 35)), Chopstick(290, (WIDTH//2 - 55, HEIGHT//2 - 35))]
+chopstick_4 = Chopstick(290, (WIDTH // 2 - 55, HEIGHT // 2 - 35))
+chopstick4States = [Chopstick(290, (WIDTH // 2 - 75, HEIGHT // 2 - 35)), # soldaki adamın sol eli
+                    Chopstick(290, (WIDTH // 2 - 55, HEIGHT // 2 - 35)), # normali
+                    Chopstick(250, (WIDTH // 2 - 55, HEIGHT // 2 - 65))] # 0 ın sağ eli
+
+# TEST ALANI
+chopstick_alpha = Chopstick(45, (WIDTH // 2 - 0, HEIGHT // 2 - 0))
+# Chopsticklerde rotasyona 45 diyince sola 45 derece dönüyor ve tam yukarı bakıyor.
+# 225 ise tam sağa bakıyor.
+# Height2 yi arttırmak aşağıya ve Width/2 yi arttırmak sağa gitmesi demek.
+
+# bu aşağıdaki 2 taneyi kullanmayacağız o7
+chopstick_philosopher3_left = Chopstick(35, (WIDTH // 2 -20, HEIGHT // 2 + 30)) # yeşil uzaylı
+chopstick_philosopher3_right = Chopstick(65, (WIDTH // 2 + 20, HEIGHT // 2 + 30))
+# bununla konumları test ettim hangisinin hangisi olduğunu artık biliyoruz.
+chopstick_test = Chopstick(290, (WIDTH // 2 - 75, HEIGHT // 2 - 35))
+
+
+
+
+
+
 
 philosopher_group.add(
     [
@@ -157,7 +197,7 @@ philosopher_group.add(
         philosopher_3,
         philosopher_4,
         chair_3,
-        chopstick_0, chopstick_1, chopstick_2, chopstick_3, chopstick_4
+        chopstick_0, chopstick_1, chopstick_2, chopstick_3, chopstick_4, chopstick_test
     ]
 )
 '''
@@ -180,11 +220,14 @@ while True:
             sys.exit()
         if event.type == pygame.MOUSEBUTTONUP:
             pass
-
-
     background_group.draw(screen)
     screen.blit(title_text.text_surface, title_text.text_rect)
     meal_group.draw(screen)
     philosopher_group.draw(screen)
+    #screen.blit(meal0_size.text_surface, meal0_size.text_rect)
+    #screen.blit(meal1_size.text_surface, meal1_size.text_rect)
+    #screen.blit(meal2_size.text_surface, meal2_size.text_rect)
+    #screen.blit(meal3_size.text_surface, meal3_size.text_rect)
+    #screen.blit(meal4_size.text_surface, meal4_size.text_rect)
     pygame.display.update()
     clock.tick(60)
--- semaphore_visual.py
+++ semaphore_visual.py
@@ -1,7 +1,7 @@
 from threading import Thread, Semaphore
 import random
 import time
-import dining_philosophers as visuals
+#import dining_philosophers as visuals
 
 
 '''
@@ -13,10 +13,9 @@ tutma durumunu kaydetme?
 
 class DiningPhilosophers:
     def __init__(self, number_of_philosophers, meal_size=9):
-        self.meal_sizes = [meal_size for _ in range(number_of_philosophers)] # yemekler ayarlanmış.
-        self.chopstick = [Semaphore(value=1) for _ in range(number_of_philosophers)]
+        self.meals = [meal_size for _ in range(number_of_philosophers)] # yemekler ayarlanmış.
+        self.chopsticks = [Semaphore(value=1) for _ in range(number_of_philosophers)]
         self.status = ['WAITING' for _ in range(number_of_philosophers)]
-        #chopstick tutma listesi yeniden tanımlanmalı.
         # yeni liste her bir stick için 1 artırıcak. 2 durumu yeme, 1 durumu tek stickle bekleme 0 durumu ise sticksiz.
         self.chopstick_holders = [0 for _ in range(number_of_philosophers)]
         self.number_of_philosophers = number_of_philosophers
@@ -29,36 +28,40 @@ class DiningPhilosophers:
             self.status[i] = '  _  '
             # chopstick aldığında sol tarafına alıyor ve tutmaya sol kısımı ekliyor.
             if self.chopsticks[i].acquire(timeout=1):
-                self.chopstick_holders[i] = ' /   '
-                #belli süre bekliyor.
+                self.chopstick_holders[i] += 1
+                # SOLUNA CHOPSTICK GELMELİ
                 time.sleep(random.random())
                 # 2. chopstick i alıyor.
                 if self.chopsticks[j].acquire(timeout=1):
-                    self.chopstick_holders[i] = ' / \\ ' #2 elle tutuyor.
+                    self.chopstick_holders[i] += 1
                     self.status[i] = '  E  '  # yeme durumuna geçiyor.
-                    time.sleep(random.random())  # bekliyor.
-                    self.meals[i] -= 1 # yemeği azalıyor.
+                    time.sleep(random.random())
+                    self.meals[i] -= 1  # yemeği azalıyor.
                     self.chopsticks[j].release()  # chopstick in birini bırakıyor.
-                    self.chopstick_holders[i] = ' /   '  # sağdaki gitti.
+                    self.chopstick_holders[i] -= 1
                 self.chopsticks[i].release()  # diğerini bırakıyor.
-                self.chopstick_holders[i] = '     '  # 2side yok.
+                self.chopstick_holders[i] = 0
                 self.status[i] = '  T  '  # boşta durumuna geçiyor.
 
 
 def main():
     # n filozof sayısı
     n = 5
-    # m yemeğin büyüklüğü
-    m = 7
-    dining_philosophers = DiningPhilosophers(n, m)
+    meal_size = 7
+    dining_philosophers = DiningPhilosophers(n, meal_size)
     # philosphers listesi içine filozoflar kadar thread oluşturuyor.
     philosophers = [Thread(target=dining_philosophers.philosopher, args=(i,)) for i in range(n)]
     # threadleri başlatıyor.
     for philosopher in philosophers:
         philosopher.start()
     while sum(dining_philosophers.meals) > 0:
-        #burada console da sürekli olarak görsel güncelleme yapılması sağlanmış.
-        # buna cidden ihtiyaç varmı?
+        # update the screen
+        print("=" * (n * 5))
+        print("".join(map(str, dining_philosophers.status)), " : ",
+              str(dining_philosophers.status.count('  E  ')))
+        print("".join(map(str, dining_philosophers.chopstick_holders)))
+        print("".join("{:3d}  ".format(m) for m in dining_philosophers.meals), " : ",
+              str(sum(dining_philosophers.meals)))
         time.sleep(0.1)
     for philosopher in philosophers:
         philosopher.join()
